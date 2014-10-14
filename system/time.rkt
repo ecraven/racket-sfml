12bb0d6a1f32e299c5c7d4e8575ad7cf484d8e9f
Peter Feigl <craven@gmx.net>
2014-10-14 09:49:55 +0200

    adding missing time functions

--- system/time.rkt
+++ system/time.rkt
@@ -12,3 +12,8 @@
   ([asSeconds (_fun _sfTime  -> _float)]
    [asMilliseconds (_fun _sfTime  -> _int32)]
    [asMicroseconds (_fun _sfTime  -> _int64)]))
+
+(defsys sfSeconds (_fun _float -> _sfTime))
+(defsys sfMilliseconds (_fun _int32 -> _sfTime))
+(defsys sfMicroseconds (_fun _int64 -> _sfTime))
+(defsys sfTime_Zero _sfTime)
